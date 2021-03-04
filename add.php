624f94a40bc4b8c78c32478423e01db5f5ca9335
Ramitsan <ramitsan@gmail.com>
2021-03-04 15:22:44 +0300

    Добавляет валидацию полей формы

--- add.php
+++ add.php
@@ -12,38 +12,55 @@ $errors = [];
 
 if (isset($_POST['submit'])) {
 
+// валидация обязательных полей
     foreach ($required_fields as $field) {
         if (empty($_POST[$field])) {
             $errors[$field] = 'Поле не заполнено';
         }
-    }
+    };
 
+// валидация поля с названием задачи на количество символов
+    if(isset($_POST['name'])) {
+        $min_char = 3;
+        $max_char = 50;
+        $len = strlen($_POST['name']);
+
+        if ($len < $min_char or $len > $max_char) {
+           $errors['name'] = 'Длина поля должна быть от 3 до 50 символов';
+        }
+    };
+
+// валидация поля даты
+     if(isset($_POST['date'])) {
+         $current_date = date('Ymd');
+         if($_POST['date'] < $current_date) {
+             $errors['date'] = 'Дата выполнения задачи должна быть больше или равна текущей.';
+         } else {
+            date_create_from_format('j-M-Y', $_POST['date']);
+         }
+     };
+
+// валидация файлового поля
     if (isset($_FILES['file'])) {
         $file_name = $_FILES['file']['name'];
         $file_path = __DIR__ . '/uploads/';
         $file_url = '/uploads/' . $file_name;
 
-        move_uploaded_file($_FILES['file']['tmp_name'], $file_path . $file_name);
-
-        print("<a href='$file_url'>$file_name</a>");
+        move_uploaded_file($_FILES['file']['tmp_name'], $file_path.$file_name);
+        // print("<a href='$file_url'>$file_name</a>");
     } else {
         $file_url = '';
-    }
+    };
 
     if (empty($errors)) {
         add_task($con, $_POST['name'], $_POST['project'], $_POST['date'], $file_url);
+        header('Location: index.php');
         exit;
       };
 }
 
-// // показать ошибку валидации
-// if (count($errors)) {
-//     print_r($errors[$field]);
-// }
-
 $main_content = include_template('form_task.php', ['projects' => $projects, 'errors' => $errors]);
 
 $layout = include_template('layout.php', ['main_content' => $main_content, 'title' => $title]);
-
 
 print($layout);
--- templates/form_task.php
+++ templates/form_task.php
@@ -41,7 +41,7 @@
           <div class="form__row">
             <label class="form__label" for="name">Название <sup>*</sup></label>
 
-            <input class="form__input <?php isset($errors['name']) ? print 'form__input--error' : print ''; ?>" type="text" name="name" id="name" value="<? htmlspecialchars(getPostVal('name')); ?>" placeholder="Введите название">
+            <input class="form__input <?php isset($errors['name']) ? print 'form__input--error' : print ''; ?>" type="text" name="name" id="name" value="<?=htmlspecialchars(getPostVal('name'));?>" placeholder="Введите название">
             <p class="form__message"><?= $errors['name'] ?></p>
           </div>
 
@@ -50,7 +50,7 @@
 
             <select class="form__input form__input--select <?php isset($errors['project']) ? print 'form__input--error' : print ''; ?>" name="project" id="project">
               <?php foreach ($projects as $project) : ?>
-                <option value="<?= $project['id'] ?>"><?= $project['project_title'] ?></option>
+                <option value="<?=$project['id'];?>"><?=$project['project_title'];?></option>
               <?php endforeach; ?>
             </select>
             <p class="form__message"><?= $errors['project'] ?></p>
@@ -59,7 +59,7 @@
           <div class="form__row">
             <label class="form__label" for="date">Дата выполнения</label>
 
-            <input class="form__input form__input--date <?php isset($errors['date']) ? print 'form__input--error' : print ''; ?>" type="text" name="date" id="date" value="<? getPostVal('date'); ?>" placeholder="Введите дату в формате ГГГГ-ММ-ДД">
+            <input class="form__input form__input--date <?php isset($errors['date']) ? print 'form__input--error' : print ''; ?>" type="text" name="date" id="date" value="<?=getPostVal('date');?>" placeholder="Введите дату в формате ГГГГ-ММ-ДД">
             <p class="form__message"><?= $errors['date'] ?></p>
           </div>
 
