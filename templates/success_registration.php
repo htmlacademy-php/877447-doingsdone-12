f2a7f7861253eea09547d6b558e6259fc58f50dc
Ramitsan <ramitsan@gmail.com>
2021-09-20 22:52:03 +0300

    Добавляет вывод сообщения об успешной регисторации

---
+++ templates/success_registration.php
@@ -0,0 +1,15 @@
+<div class="page-wrapper">
+  <div class="container">
+    <div class="content">
+      <section class="welcome">
+        <h2 class="welcome__heading">«Дела в порядке»</h2>
+
+        <div class="welcome__text">
+            <p>Поздравляем! Вы успешно зарегистрировались!</p>
+        </div>
+
+        <a class="welcome__button button" href="../auth.php">Войти</a>
+      </section>
+    </div>
+  </div>
+</div>
